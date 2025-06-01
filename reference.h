7dde1ea203b4a7a8a246556607cbdcdfe66308ce
odessey159 <atom20221026@gmail.com>
2025-06-01 18:21:19 +0800

    Add files via upload

---
+++ reference.h
@@ -0,0 +1,11 @@
+#ifndef REFERENCE_H
+#define REFERENCE_H
+
+#include <string>
+#include <vector>
+
+namespace reference {
+    int FindShortestPath(std::string start, std::string end, std::vector<std::string>& path, int** eWeights);
+}
+
+#endif
