61aa12336e7f62e0544b2a73b37797ef1ca8cbc0
odessey159 <atom20221026@gmail.com>
2026-03-31 17:58:17 +0800

    Delete util.h

--- util.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <string>
-#include <vector>
-
-std::tuple<bool, std::string> check_path(const std::vector<std::string>& path);
-std::vector<int> partial_length(const std::vector<std::string> &path, int** w);
-bool goes_up(const std::vector<std::string> &path, std::string start, std::string end);
